f219f6ebab89584a9217922ba43f017e587ec696
Johnny <asd28618371@gmail.com>
2022-05-15 11:16:39 +0800

    新增 頁面選單元件

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,67 @@
+<template>
+        <div class="col-md-5 d-md-block d-none">
+          <div class="Sidebar-card bg-white border border-dark border-2 px-4 py-5">
+            <div class="d-grid gap-2 mb-4">
+              <router-link to="/post" class="btn btn-primary shadow-black rounded-3
+              border-dark">
+                張貼動態
+              </router-link>
+            </div>
+            <ul class="ps-0">
+              <li class="card h-100 border-0 mb-4">
+                <a href="#">
+                  <div class="d-flex align-items-center">
+                    <img
+                      class="mx-3 img-fluid"
+                      src="~@/assets/img/user1.png"
+                      alt="user1"
+                    />
+                    <p class="mt-2 fw-bold">邊緣小杰</p>
+                  </div>
+                </a>
+              </li>
+              <li class="card h-100 border-0 mb-4">
+                  <router-link to="/follower" class="hover-primary">
+                  <div class="media d-flex align-items-center">
+                    <div
+                      class="btn-icon rounded-circle border border-dark border-2 p-2 mx-3 bg-light
+                      d-flex justify-content-center align-items-center"
+                    >
+                      <i class="bi bi-bell fs-5"></i>
+                    </div>
+                    <p class="my-2 fw-bold">追蹤名單</p>
+                  </div>
+                  </router-link>
+              </li>
+              <li class="card h-100 border-0">
+                <router-link to="/like" class="hover-primary">
+                  <div class="media d-flex align-items-center">
+                    <div
+                      class="btn-icon rounded-circle border border-dark border-2 p-2 mx-3 bg-light hover-primary
+                      d-flex justify-content-center align-items-center"
+                    >
+                      <i class="bi bi-hand-thumbs-up fs-5"></i>
+                    </div>
+                    <p class="my-2 fw-bold">我按讚的文章</p>
+                  </div>
+              </router-link>
+              </li>
+            </ul>
+          </div>
+        </div>
+</template>
+
+<script>
+export default {
+  name: 'Sidebar'
+}
+</script>
+
+<style lang="scss">
+.Sidebar-card {
+  .btn-icon {
+    width: 50px;
+    height: 50px;
+  }
+}
+</style>
---
+++ src/components/SidebarSm.vue
@@ -0,0 +1,51 @@
+<template>
+      <div
+        class="SidebarSm list-group border border-dark rounded-pill fixed-bottom bg-secondary d-md-none mx-1 mb-2"
+      >
+        <ul class="d-flex justify-content-around mb-0 ps-0">
+          <li class="list-group-item border-0 bg-secondary">
+            <a href="#">
+              <div class="btn rounded-circle border border-dark border-2 p-2 bg-white">
+                <i class="bi bi-house-door fs-5"></i>
+              </div>
+            </a>
+          </li>
+          <li class="list-group-item border-0 bg-secondary">
+            <a href="#">
+              <div class="btn rounded-circle border border-dark border-2 p-2 bg-white">
+                <i class="bi bi-bell fs-5"></i>
+              </div>
+            </a>
+          </li>
+          <li class="list-group-item border-0 bg-secondary">
+            <a href="#">
+              <div class="btn rounded-circle border border-dark border-2 p-2 bg-white">
+                <i class="bi bi-hand-thumbs-up fs-5"></i>
+              </div>
+            </a>
+          </li>
+          <li class="list-group-item border-0 bg-secondary">
+            <a href="#">
+              <div class="btn rounded-circle border border-dark border-2 p-2 bg-primary">
+                <i class="bi bi-plus-circle text-white fs-5"></i>
+              </div>
+            </a>
+          </li>
+        </ul>
+      </div>
+</template>
+
+<script>
+export default {
+  name: 'SidebarSm'
+}
+</script>
+
+<style lang="scss">
+.SidebarSm {
+  .btn {
+    width: 3rem;
+    height: 3rem;
+  }
+}
+</style>
