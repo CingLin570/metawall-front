7cc64722da207027eb19cb63a932a4f02f88c9f4
Johnny <asd28618371@gmail.com>
2022-05-15 11:17:02 +0800

    新增 路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,70 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import Home from '../views/Home.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: '首頁',
+    component: () => import('../views/Home.vue'),
+    children: [
+      {
+        path: '',
+        name: '貼文牆頁',
+        component: () => import('../views/Wall.vue')
+      },
+      {
+        path: 'personal',
+        name: '個人貼文牆頁',
+        component: () => import('../views/Personal.vue')
+      },
+      {
+        path: '/profile',
+        name: '個人資料頁',
+        component: () => import('../views/Profile.vue')
+      },
+      {
+        path: '/like',
+        name: '我按讚文章頁',
+        component: () => import('../views/Like.vue')
+      },
+      {
+        path: '/follower',
+        name: '追隨名單頁',
+        component: () => import('../views/Follower.vue')
+      },
+      {
+        path: '/post',
+        name: '張貼動態頁',
+        component: () => import('../views/Post.vue')
+      }
+    ]
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/login',
+    name: '登入頁面',
+    component: () => import('../views/login/Login.vue')
+  },
+  {
+    path: '/register',
+    name: '註冊頁面',
+    component: () => import('../views/login/Register.vue')
+  },
+  // 重新導向
+  {
+    path: '/:pathMatch(.*)*',
+    redirect: {
+      name: '貼文牆頁'
+    }
   }
 ]
 
 const router = createRouter({
   history: createWebHashHistory(),
-  routes
+  routes,
+  scrollBehavior (to, from, scrollBehavior) {
+    return {
+      top: 0
+    }
+  }
 })
 
 export default router
