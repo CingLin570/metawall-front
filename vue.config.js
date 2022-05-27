ede4fbfab640523e03ce4822c70b47c8ed4547f2
Johnny <asd28618371@gmail.com>
2022-05-27 10:43:39 +0800

    修改 設定

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
-
+const path = require('path')
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? './' : './',
-  outputDir: 'dist'
+  outputDir: 'dist',
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src')
+      }
+    }
+  }
 }
