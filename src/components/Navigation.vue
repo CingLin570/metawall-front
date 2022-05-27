1d6ce07af3c0c2f3030db5053fbbebe90a147419
Johnny <asd28618371@gmail.com>
2022-05-27 10:55:13 +0800

    新增 拆出選單及列表元件

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,51 +1,96 @@
 <template>
-    <section>
-      <nav
-        class="navbar navbar-expand-lg navbar-light fixed-top border-bottom border-3 border-dark
-        bg-white"
-      >
-        <div class="container px-lg-12">
-          <h1 class="font-paytone">
-            <a class="navbar-brand fs-1" href="#">MetaWall</a>
-          </h1>
-          <div class="d-flex align-items-center">
-            <img src="~@/assets/img/user1.png" alt="user1" class="me-2 img-fluid" />
-            <div class="dropdown bg-white me-2">
-              <button
-                class="btn btn-white border-0 text-decoration-underline fw-bolder font-azeret"
-                type="button"
-                id="dropdownMenuButton1"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Member
-              </button>
-              <ul
-                class="dropdown-menu py-0 border border-dark border-2 rounded-0
-                menu-shadow"
-                aria-labelledby="dropdownMenuButton1"
-              >
-                <li class="border-bottom border-dark border-2">
-                  <router-link to="/personal" class="dropdown-item">我的貼文牆</router-link>
-                </li>
-                <li class="border-bottom border-dark border-2">
-                  <router-link to="/profile" class="dropdown-item">修改個人資料</router-link>
-                </li>
-                <li class="border-dark border-2">
-                  <router-link to="/login" class="dropdown-item">登出</router-link>
-                </li>
-              </ul>
-            </div>
+  <section>
+    <nav
+      class="
+        navbar navbar-expand-lg navbar-light
+        fixed-top
+        border-bottom border-3 border-dark
+        bg-white
+      "
+    >
+      <div class="container px-lg-12">
+        <h1 class="font-paytone">
+          <a class="navbar-brand fs-1" href="#">MetaWall</a>
+        </h1>
+        <div class="d-flex align-items-center">
+          <img
+            :src="info.photo"
+            alt="user1"
+            class="me-2 img-fluid"
+            v-if="info.photo"
+          />
+          <div class="dropdown bg-white me-2">
+            <button
+              class="
+                btn btn-white
+                border-0
+                text-decoration-underline
+                fw-bolder
+                font-azeret
+              "
+              type="button"
+              id="dropdownMenuButton1"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              Member
+            </button>
+            <ul
+              class="
+                dropdown-menu
+                py-0
+                border border-dark border-2
+                rounded-0
+                menu-shadow
+              "
+              aria-labelledby="dropdownMenuButton1"
+            >
+              <li class="border-bottom border-dark border-2">
+                <router-link to="/personal" class="dropdown-item"
+                  >我的貼文牆</router-link
+                >
+              </li>
+              <li class="border-bottom border-dark border-2">
+                <router-link to="/profile" class="dropdown-item"
+                  >修改個人資料</router-link
+                >
+              </li>
+              <li class="border-dark border-2">
+                <router-link to="/login" class="dropdown-item"
+                @click.prevent="logout"
+                  >登出</router-link
+                >
+              </li>
+            </ul>
           </div>
         </div>
-      </nav>
-    </section>
-    <div class="py-3 py-md-5"></div>
+      </div>
+    </nav>
+  </section>
+  <div class="py-3 py-md-5"></div>
 </template>
 
 <script>
+import { mapState, mapMutations } from 'vuex'
 export default {
-
+  name: 'Navigation',
+  data () {
+    return {
+    }
+  },
+  computed: {
+    ...mapState({
+      info: state => state.info
+    })
+  },
+  methods: {
+    ...mapMutations(['setInfo', 'setToken']),
+    logout () {
+      this.setToken()
+      this.setInfo()
+      this.$router.push({ name: '登入頁面' })
+    }
+  }
 }
 </script>
 
--- src/components/SidebarSm.vue
+++ src/components/SidebarSm.vue
@@ -1,38 +1,62 @@
 <template>
-      <div
-        class="SidebarSm list-group border border-dark rounded-pill fixed-bottom bg-secondary d-md-none mx-1 mb-2"
-      >
-        <ul class="d-flex justify-content-around mb-0 ps-0">
-          <li class="list-group-item border-0 bg-secondary">
-            <a href="#">
-              <div class="btn rounded-circle border border-dark border-2 p-2 bg-white">
-                <i class="bi bi-house-door fs-5"></i>
-              </div>
-            </a>
-          </li>
-          <li class="list-group-item border-0 bg-secondary">
-            <a href="#">
-              <div class="btn rounded-circle border border-dark border-2 p-2 bg-white">
-                <i class="bi bi-bell fs-5"></i>
-              </div>
-            </a>
-          </li>
-          <li class="list-group-item border-0 bg-secondary">
-            <a href="#">
-              <div class="btn rounded-circle border border-dark border-2 p-2 bg-white">
-                <i class="bi bi-hand-thumbs-up fs-5"></i>
-              </div>
-            </a>
-          </li>
-          <li class="list-group-item border-0 bg-secondary">
-            <a href="#">
-              <div class="btn rounded-circle border border-dark border-2 p-2 bg-primary">
-                <i class="bi bi-plus-circle text-white fs-5"></i>
-              </div>
-            </a>
-          </li>
-        </ul>
-      </div>
+  <div
+    class="
+      SidebarSm
+      list-group
+      border border-dark
+      rounded-pill
+      fixed-bottom
+      bg-secondary
+      d-md-none
+      mx-1
+      mb-2
+    "
+  >
+    <ul class="d-flex justify-content-around mb-0 ps-0">
+      <li class="list-group-item border-0 bg-secondary">
+        <a href="#">
+          <div
+            class="btn rounded-circle border border-dark border-2 p-2 bg-white"
+          >
+            <i class="bi bi-house-door fs-5"></i>
+          </div>
+        </a>
+      </li>
+      <li class="list-group-item border-0 bg-secondary">
+        <a href="#">
+          <div
+            class="btn rounded-circle border border-dark border-2 p-2 bg-white"
+          >
+            <i class="bi bi-bell fs-5"></i>
+          </div>
+        </a>
+      </li>
+      <li class="list-group-item border-0 bg-secondary">
+        <a href="#">
+          <div
+            class="btn rounded-circle border border-dark border-2 p-2 bg-white"
+          >
+            <i class="bi bi-hand-thumbs-up fs-5"></i>
+          </div>
+        </a>
+      </li>
+      <li class="list-group-item border-0 bg-secondary">
+        <a href="#">
+          <div
+            class="
+              btn
+              rounded-circle
+              border border-dark border-2
+              p-2
+              bg-primary
+            "
+          >
+            <i class="bi bi-plus-circle text-white fs-5"></i>
+          </div>
+        </a>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
