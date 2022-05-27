1cdecde5785ac5d0abf28f316112886c2acb64ca
Johnny <asd28618371@gmail.com>
2022-05-27 10:52:50 +0800

    新增 我按讚文章頁

--- src/views/Like.vue
+++ src/views/Like.vue
@@ -1,171 +1,195 @@
 <template>
-    <section class="container mt-7 px-lg-12">
-      <div class="row">
-        <div class="col-md-7">
-          <div class="card mb-5 rounded-0 border-2 position-relative">
-            <h2 class="text-center fw-bold py-3 mb-0 fs-5 fw-bold">追蹤名單</h2>
-            <div
-              class="position-absolute border border-dark border-2 w-100 py-4"
-              style="z-index: -1; top: 10px; left: -6px"
-            ></div>
-          </div>
-          <ul class="ps-0">
-            <li class="card h-100 py-3 px-4 mb-3 border-2 shadow-black">
-              <div class="d-flex justify-content-between align-items-center">
-                <div class="d-flex align-items-center">
-                  <img
-                    class="me-3 img-fluid"
-                    src="~@/assets/img/user2.png"
-                    alt="user2"
-                  />
-                  <div class="d-flex flex-column mt-2">
-                    <a href="#" class="mb-0 fw-bold">愛爾敏</a>
-                    <small class="text-muted">發文時間：2022/2/22 12:00</small>
-                  </div>
-                </div>
-                <div class="d-flex align-items-center">
-                  <button type="button" class="btn d-flex flex-column">
-                    <i class="bi bi-hand-thumbs-up fs-4 text-primary mb-1 mx-auto"></i>
-                    <span class="fs-5 fw-bold">取消</span>
-                  </button>
-                  <button type="button" class="btn d-flex flex-column">
-                    <i class="bi bi-arrow-right-circle fs-4 fw-bold mb-1 mx-auto"></i>
-                    <span class="fs-5 fw-bold">查看</span>
-                  </button>
-                </div>
-              </div>
-            </li>
-            <li class="card h-100 py-3 px-4 mb-3 border-2 shadow-black">
-              <div class="d-flex justify-content-between align-items-center">
-                <div class="d-flex align-items-center">
-                  <img
-                    class="me-3 img-fluid"
-                    src="~@/assets/img/user5.png"
-                    alt="user5"
-                  />
-                  <div class="d-flex flex-column mt-2">
-                    <a href="#" class="mb-0 fw-bold">愛爾敏</a>
-                    <small class="text-muted">發文時間：2022/2/22 12:00</small>
-                  </div>
-                </div>
-                <div class="d-flex align-items-center">
-                  <button type="button" class="btn d-flex flex-column">
-                    <i class="bi bi-hand-thumbs-up fs-4 text-primary mb-1 mx-auto"></i>
-                    <span class="fs-5 fw-bold">取消</span>
-                  </button>
-                  <button type="button" class="btn d-flex flex-column">
-                    <i class="bi bi-arrow-right-circle fs-4 fw-bold mb-1 mx-auto"></i>
-                    <span class="fs-5 fw-bold">查看</span>
-                  </button>
-                </div>
-              </div>
-            </li>
-            <li class="card h-100 py-3 px-4 mb-3 border-2 shadow-black">
-              <div class="d-flex justify-content-between align-items-center">
-                <div class="d-flex align-items-center">
-                  <img
-                    class="me-3 img-fluid"
-                    src="~@/assets/img/user3.png"
-                    alt="user3"
-                  />
-                  <div class="d-flex flex-column mt-2">
-                    <a href="#" class="mb-0 fw-bold">米卡莎</a>
-                    <small class="text-muted">發文時間：2022/2/22 12:00</small>
-                  </div>
-                </div>
-                <div class="d-flex align-items-center">
-                  <button type="button" class="btn d-flex flex-column">
-                    <i class="bi bi-hand-thumbs-up fs-4 text-primary mb-1 mx-auto"></i>
-                    <span class="fs-5 fw-bold">取消</span>
-                  </button>
-                  <button type="button" class="btn d-flex flex-column">
-                    <i class="bi bi-arrow-right-circle fs-4 fw-bold mb-1 mx-auto"></i>
-                    <span class="fs-5 fw-bold">查看</span>
-                  </button>
-                </div>
-              </div>
-            </li>
-            <li class="card h-100 py-3 px-4 mb-3 border-2 shadow-black">
-              <div class="d-flex justify-content-between align-items-center">
-                <div class="d-flex align-items-center">
-                  <img
-                    class="me-3 img-fluid"
-                    src="~@/assets/img/user6.png"
-                    alt="user6"
-                  />
-                  <div class="d-flex flex-column mt-2">
-                    <a href="#" class="mb-0 fw-bold">李維</a>
-                    <small class="text-muted">發文時間：2022/2/22 12:00</small>
-                  </div>
-                </div>
-                <div class="d-flex align-items-center">
-                  <button type="button" class="btn d-flex flex-column">
-                    <i class="bi bi-hand-thumbs-up fs-4 text-primary mb-1 mx-auto"></i>
-                    <span class="fs-5 fw-bold">取消</span>
-                  </button>
-                  <button type="button" class="btn d-flex flex-column">
-                    <i class="bi bi-arrow-right-circle fs-4 fw-bold mb-1 mx-auto"></i>
-                    <span class="fs-5 fw-bold">查看</span>
-                  </button>
-                </div>
-              </div>
-            </li>
-            <li class="card h-100 py-3 px-4 mb-3 border-2 shadow-black">
-              <div class="d-flex justify-content-between align-items-center">
-                <div class="d-flex align-items-center">
-                  <img
-                    class="me-3 img-fluid"
-                    src="~@/assets/img/user5.png"
-                    alt="user5"
-                  />
-                  <div class="d-flex flex-column mt-2">
-                    <a href="#" class="mb-0 fw-bold">米歇爾</a>
-                    <small class="text-muted">發文時間：2022/2/22 12:00</small>
-                  </div>
-                </div>
-                <div class="d-flex align-items-center">
-                  <button type="button" class="btn d-flex flex-column">
-                    <i class="bi bi-hand-thumbs-up fs-4 text-primary mb-1 mx-auto"></i>
-                    <span class="fs-5 fw-bold">取消</span>
-                  </button>
-                  <button type="button" class="btn d-flex flex-column">
-                    <i class="bi bi-arrow-right-circle fs-4 fw-bold mb-1 mx-auto"></i>
-                    <span class="fs-5 fw-bold">查看</span>
-                  </button>
-                </div>
-              </div>
-            </li>
-            <li class="card h-100 py-3 px-4 mb-3 border-2 shadow-black">
-              <div class="d-flex justify-content-between align-items-center">
-                <div class="d-flex align-items-center">
-                  <img
-                    class="me-3 img-fluid"
-                    src="~@/assets/img/user2.png"
-                    alt="user2"
-                  />
-                  <div class="d-flex flex-column mt-2">
-                    <a href="#" class="mb-0 fw-bold">愛爾敏</a>
-                    <small class="text-muted">發文時間：2022/2/22 12:00</small>
-                  </div>
-                </div>
-                <div class="d-flex align-items-center">
-                  <button type="button" class="btn d-flex flex-column">
-                    <i class="bi bi-hand-thumbs-up fs-4 text-primary mb-1 mx-auto"></i>
-                    <span class="fs-5 fw-bold">取消</span>
-                  </button>
-                  <button type="button" class="btn d-flex flex-column">
-                    <i class="bi bi-arrow-right-circle fs-4 fw-bold mb-1 mx-auto"></i>
-                    <span class="fs-5 fw-bold">查看</span>
-                  </button>
-                </div>
-              </div>
-            </li>
-          </ul>
+  <section class="container mt-7 px-lg-12">
+    <div class="row">
+      <div class="col-md-7">
+        <div class="card mb-5 rounded-0 border-2 position-relative">
+          <h2 class="text-center fw-bold py-3 mb-0 fs-5 fw-bold">追蹤名單</h2>
+          <div
+            class="position-absolute border border-dark border-2 w-100 py-4"
+            style="z-index: -1; top: 10px; left: -6px"
+          ></div>
         </div>
-        <Sidebar></Sidebar>
+        <ul class="ps-0">
+          <li class="card h-100 py-3 px-4 mb-3 border-2 shadow-black">
+            <div class="d-flex justify-content-between align-items-center">
+              <div class="d-flex align-items-center">
+                <img
+                  class="me-3 img-fluid"
+                  src="~@/assets/img/user2.png"
+                  alt="user2"
+                />
+                <div class="d-flex flex-column mt-2">
+                  <a href="#" class="mb-0 fw-bold">愛爾敏</a>
+                  <small class="text-muted">發文時間：2022/2/22 12:00</small>
+                </div>
+              </div>
+              <div class="d-flex align-items-center">
+                <button type="button" class="btn d-flex flex-column">
+                  <i
+                    class="bi bi-hand-thumbs-up fs-4 text-primary mb-1 mx-auto"
+                  ></i>
+                  <span class="fs-5 fw-bold">取消</span>
+                </button>
+                <button type="button" class="btn d-flex flex-column">
+                  <i
+                    class="bi bi-arrow-right-circle fs-4 fw-bold mb-1 mx-auto"
+                  ></i>
+                  <span class="fs-5 fw-bold">查看</span>
+                </button>
+              </div>
+            </div>
+          </li>
+          <li class="card h-100 py-3 px-4 mb-3 border-2 shadow-black">
+            <div class="d-flex justify-content-between align-items-center">
+              <div class="d-flex align-items-center">
+                <img
+                  class="me-3 img-fluid"
+                  src="~@/assets/img/user5.png"
+                  alt="user5"
+                />
+                <div class="d-flex flex-column mt-2">
+                  <a href="#" class="mb-0 fw-bold">愛爾敏</a>
+                  <small class="text-muted">發文時間：2022/2/22 12:00</small>
+                </div>
+              </div>
+              <div class="d-flex align-items-center">
+                <button type="button" class="btn d-flex flex-column">
+                  <i
+                    class="bi bi-hand-thumbs-up fs-4 text-primary mb-1 mx-auto"
+                  ></i>
+                  <span class="fs-5 fw-bold">取消</span>
+                </button>
+                <button type="button" class="btn d-flex flex-column">
+                  <i
+                    class="bi bi-arrow-right-circle fs-4 fw-bold mb-1 mx-auto"
+                  ></i>
+                  <span class="fs-5 fw-bold">查看</span>
+                </button>
+              </div>
+            </div>
+          </li>
+          <li class="card h-100 py-3 px-4 mb-3 border-2 shadow-black">
+            <div class="d-flex justify-content-between align-items-center">
+              <div class="d-flex align-items-center">
+                <img
+                  class="me-3 img-fluid"
+                  src="~@/assets/img/user3.png"
+                  alt="user3"
+                />
+                <div class="d-flex flex-column mt-2">
+                  <a href="#" class="mb-0 fw-bold">米卡莎</a>
+                  <small class="text-muted">發文時間：2022/2/22 12:00</small>
+                </div>
+              </div>
+              <div class="d-flex align-items-center">
+                <button type="button" class="btn d-flex flex-column">
+                  <i
+                    class="bi bi-hand-thumbs-up fs-4 text-primary mb-1 mx-auto"
+                  ></i>
+                  <span class="fs-5 fw-bold">取消</span>
+                </button>
+                <button type="button" class="btn d-flex flex-column">
+                  <i
+                    class="bi bi-arrow-right-circle fs-4 fw-bold mb-1 mx-auto"
+                  ></i>
+                  <span class="fs-5 fw-bold">查看</span>
+                </button>
+              </div>
+            </div>
+          </li>
+          <li class="card h-100 py-3 px-4 mb-3 border-2 shadow-black">
+            <div class="d-flex justify-content-between align-items-center">
+              <div class="d-flex align-items-center">
+                <img
+                  class="me-3 img-fluid"
+                  src="~@/assets/img/user6.png"
+                  alt="user6"
+                />
+                <div class="d-flex flex-column mt-2">
+                  <a href="#" class="mb-0 fw-bold">李維</a>
+                  <small class="text-muted">發文時間：2022/2/22 12:00</small>
+                </div>
+              </div>
+              <div class="d-flex align-items-center">
+                <button type="button" class="btn d-flex flex-column">
+                  <i
+                    class="bi bi-hand-thumbs-up fs-4 text-primary mb-1 mx-auto"
+                  ></i>
+                  <span class="fs-5 fw-bold">取消</span>
+                </button>
+                <button type="button" class="btn d-flex flex-column">
+                  <i
+                    class="bi bi-arrow-right-circle fs-4 fw-bold mb-1 mx-auto"
+                  ></i>
+                  <span class="fs-5 fw-bold">查看</span>
+                </button>
+              </div>
+            </div>
+          </li>
+          <li class="card h-100 py-3 px-4 mb-3 border-2 shadow-black">
+            <div class="d-flex justify-content-between align-items-center">
+              <div class="d-flex align-items-center">
+                <img
+                  class="me-3 img-fluid"
+                  src="~@/assets/img/user5.png"
+                  alt="user5"
+                />
+                <div class="d-flex flex-column mt-2">
+                  <a href="#" class="mb-0 fw-bold">米歇爾</a>
+                  <small class="text-muted">發文時間：2022/2/22 12:00</small>
+                </div>
+              </div>
+              <div class="d-flex align-items-center">
+                <button type="button" class="btn d-flex flex-column">
+                  <i
+                    class="bi bi-hand-thumbs-up fs-4 text-primary mb-1 mx-auto"
+                  ></i>
+                  <span class="fs-5 fw-bold">取消</span>
+                </button>
+                <button type="button" class="btn d-flex flex-column">
+                  <i
+                    class="bi bi-arrow-right-circle fs-4 fw-bold mb-1 mx-auto"
+                  ></i>
+                  <span class="fs-5 fw-bold">查看</span>
+                </button>
+              </div>
+            </div>
+          </li>
+          <li class="card h-100 py-3 px-4 mb-3 border-2 shadow-black">
+            <div class="d-flex justify-content-between align-items-center">
+              <div class="d-flex align-items-center">
+                <img
+                  class="me-3 img-fluid"
+                  src="~@/assets/img/user2.png"
+                  alt="user2"
+                />
+                <div class="d-flex flex-column mt-2">
+                  <a href="#" class="mb-0 fw-bold">愛爾敏</a>
+                  <small class="text-muted">發文時間：2022/2/22 12:00</small>
+                </div>
+              </div>
+              <div class="d-flex align-items-center">
+                <button type="button" class="btn d-flex flex-column">
+                  <i
+                    class="bi bi-hand-thumbs-up fs-4 text-primary mb-1 mx-auto"
+                  ></i>
+                  <span class="fs-5 fw-bold">取消</span>
+                </button>
+                <button type="button" class="btn d-flex flex-column">
+                  <i
+                    class="bi bi-arrow-right-circle fs-4 fw-bold mb-1 mx-auto"
+                  ></i>
+                  <span class="fs-5 fw-bold">查看</span>
+                </button>
+              </div>
+            </div>
+          </li>
+        </ul>
       </div>
-      <SidebarSm></SidebarSm>
-    </section>
+      <Sidebar></Sidebar>
+    </div>
+    <SidebarSm></SidebarSm>
+  </section>
 </template>
 
 <script>
@@ -182,5 +206,4 @@ export default {
 </script>
 
 <style>
-
 </style>
