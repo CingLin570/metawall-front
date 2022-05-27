a01ac488247756edd9143fc528e557384b0496fc
Johnny <asd28618371@gmail.com>
2022-05-27 10:57:10 +0800

    新增 頁面前置守衛驗證登入狀態

--- src/router/index.js
+++ src/router/index.js
@@ -1,58 +1,54 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-
+import store from '../store/index'
 const routes = [
   {
     path: '/',
-    name: '首頁',
-    component: () => import('../views/Home.vue'),
+    component: () => import('@/views/Home.vue'),
+    meta: { requiresAuth: true },
     children: [
       {
         path: '',
-        name: '貼文牆頁',
-        component: () => import('../views/Wall.vue')
+        name: '首頁',
+        component: () => import('@/views/Wall.vue')
       },
       {
         path: 'personal',
         name: '個人貼文牆頁',
-        component: () => import('../views/Personal.vue')
+        component: () => import('@/views/Personal.vue')
       },
       {
         path: '/profile',
         name: '個人資料頁',
-        component: () => import('../views/Profile.vue')
+        component: () => import('@/views/profile/Index.vue')
       },
       {
         path: '/like',
         name: '我按讚文章頁',
-        component: () => import('../views/Like.vue')
+        component: () => import('@/views/Like.vue')
       },
       {
         path: '/follower',
         name: '追隨名單頁',
-        component: () => import('../views/Follower.vue')
+        component: () => import('@/views/Follower.vue')
       },
       {
         path: '/post',
         name: '張貼動態頁',
-        component: () => import('../views/Post.vue')
+        component: () => import('@/views/Post.vue')
       }
     ]
   },
   {
     path: '/login',
     name: '登入頁面',
-    component: () => import('../views/login/Login.vue')
-  },
-  {
-    path: '/register',
-    name: '註冊頁面',
-    component: () => import('../views/login/Register.vue')
+    meta: { requiresAuth: false },
+    component: () => import('@/views/login/Index.vue')
   },
   // 重新導向
   {
     path: '/:pathMatch(.*)*',
     redirect: {
-      name: '貼文牆頁'
+      name: '首頁'
     }
   }
 ]
@@ -64,6 +60,22 @@ const router = createRouter({
     return {
       top: 0
     }
+  }
+})
+// 使用 router.beforeEach 註冊一個全域性前置守衛，判斷使用者是否登陸
+router.beforeEach(async (to, from) => {
+  try {
+    // 判斷是否登入
+    const token = store.state.token
+    // 判斷是否需要驗證
+    const isMatched = to.matched.some(record => record.meta.requiresAuth)
+    if (!token && isMatched) {
+      return { name: '登入頁面' }
+    } else {
+      return true
+    }
+  } catch (error) {
+    return { name: '登入頁面' }
   }
 })
 
