8af481071166b1ddc286a4154b3770718851052e
Johnny <asd28618371@gmail.com>
2022-06-15 23:10:18 +0800

    新增 將loading加入全域

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 <template>
   <div id="app">
+    <Loading :active="isLoading" :z-index="9999"></Loading>
+    <router-view/>
   </div>
-  <router-view/>
 </template>
 
+<script>
+export default {
+  computed: {
+    isLoading () {
+      return this.$store.state.isLoading
+    }
+  }
+}
+</script>
 <style lang="scss">
   @import '@/assets/scss/all.scss';
 </style>
